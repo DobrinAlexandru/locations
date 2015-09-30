a7e247bdde31ffb8f9686d1a927d5334a4461c59
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2015-09-30 14:16:41 +0300

    useful script to transfer code to amazon fast

---
+++ copy-paste-amazon.sh
@@ -0,0 +1,1 @@
+cat ~/Documents/Intersect/location-api/server.js | sudo ssh -i ~/Dropbox/intersect-amazon-1.pem ubuntu@ec2-52-28-189-73.eu-central-1.compute.amazonaws.com "cat > ~/locations-api/server.js"
